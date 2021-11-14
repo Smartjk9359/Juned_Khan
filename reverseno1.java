21362fd517c7041d8e7e69805d5ae0b9adba05ff
Smartjk9359 <94166450+Smartjk9359@users.noreply.github.com>
2021-11-14 11:56:29 +0530

    Add files via upload
    New version of reverseon program

---
+++ reverseno1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class reverseno1
+{
+    public static void main(String...s)
+    {
+        //REVERSE ANY NUMBER 
+        //FOR NEGATIVE NUMBER EX -547 THEN PRINT THIS TYPE 7 4 5
+        //FOR POSITIVE NUMBER EX  563 THEN PRINT THIS TYPE 3 6 5
+      System.out.println("C_R_E_A_T_E_D B_Y J_U_N_E_D K_H_A_N ");
+      int no1,no2,i,count=0;
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter any No: ");
+      no2= no1 = in.nextInt();
+      if(no1<0)
+      {
+        no1 = no1*(-1);
+        no2 = no1;  
+      }
+      if(no1==0)
+      System.out.println("0");
+      else
+      {
+        while (no1!= 0)
+            {
+             no1 = no1 / 10;
+            ++count;
+            }
+        while(count!=0)
+            {
+            i=0;
+            int arr[ ] = {no2%10};
+             no2 = no2/10;
+            System.out.print(arr[i]);
+            i++;
+            count=count-1;
+            }
+      }
+      in.close();
+      System.out.println( );
+      System.out.println("T_H_A_N_K_S F_O_R U_S_E");
+    }
+}
